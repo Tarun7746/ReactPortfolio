036b7f503cb843ae26d7fafb139b59213bd58364
Tarun77461 <mandaltarun016@gmail.com>
2024-04-19 22:00:06 +0530

    zxcvbg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [text, setText] = useState("");
+  const [isTyping, setIsTyping] = useState(true);
+  const typingSpeed = 100; // Typing speed in milliseconds (slower)
+  const backspaceSpeed = 5; // Backspace speed in milliseconds (faster)
+
+  useEffect(() => {
+    let typingTimeout;
+    let backspaceTimeout;
+
+    const typeWriter = () => {
+      if (isTyping) {
+        typingTimeout = setTimeout(() => {
+          if (text.length < "I am Tarun".length) {
+            setText((prevText) => prevText + "I am Tarun"[text.length]);
+            typeWriter();
+          } else {
+            setIsTyping(false);
+            backspaceTimeout = setTimeout(backspaceWriter, typingSpeed * 2);
+          }
+        }, typingSpeed);
+      }
+    };
+
+    const backspaceWriter = () => {
+      backspaceTimeout = setTimeout(() => {
+        if (!isTyping && text.length > 0) {
+          setText((prevText) => prevText.slice(0, -1));
+          backspaceWriter();
+        } else {
+          setIsTyping(true);
+          setText(""); // Reset text after backspace deletion
+          typeWriter();
+        }
+      }, backspaceSpeed);
+    };
+
+    typeWriter();
+
+    // Clean-up function to clear timeouts on unmount
+    return () => {
+      clearTimeout(typingTimeout);
+      clearTimeout(backspaceTimeout);
+    };
+  }, [text, isTyping]);
+
   return (
     <>
       <div>
@@ -37,7 +83,9 @@ const Header = () => {
         <div class="header-content container">
           <h1 class="header-title">
             <span class="up">HI!</span>
-            <span class="down">I am Tarun</span>
+            <span class="down" id="text-animation">
+              {text}
+            </span>
           </h1>
           <p class="header-subtitle">FRONTEND WEB DESIGNER</p>
 
--- src/components/IndexHome.js
+++ src/components/IndexHome.js
@@ -1,12 +1,40 @@
 import React from "react";
 import Header from "./Header";
+import AboutPic from "../assets/imgs/man.png";
 
 const IndexHome = () => {
   return (
     <div>
       <Header />
-      <h1>Welcome to IndexHome</h1>
-      <p>This is the home page content.</p>
+      <div className="AboutSectiOnn">
+        <section class="section pt-0" id="about">
+          <div class="container text-center">
+            <div class="about">
+              <div class="about-img-holder">
+                <img src={AboutPic} class="about-img" alt="dss"></img>
+              </div>
+              <div class="about-caption">
+                <p class="section-subtitle">Who Am I ?</p>
+                <h2 class="section-title mb-3">About Me</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Repudiandae aliquid ad provident aut fuga animi soluta quae
+                  eos non cupiditate voluptates dolorem, doloremque quos dicta
+                  quibusdam impedit iure nemo a iste culpl Quasi quibusdam hic
+                  recusandae delectus velit officiis explicabo voluptatibus!
+                  Nemo esse similique, voluptates labore distinctio, placeat
+                  explicabo facilis molestias, blanditiis culpa iusto voluptatem
+                  ratione eligendi a, quia temporibus velit vero ipsa sint ex
+                  voluptatum expedita aliquid! Debitis, nam!
+                </p>
+                <button class="btn-rounded btn btn-outline-primary mt-4">
+                  Download CV
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
