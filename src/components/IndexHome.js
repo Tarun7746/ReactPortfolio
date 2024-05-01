7738d529268005f0ddd0df9a8185bc34ae0e0a1f
Tarun77461 <mandaltarun016@gmail.com>
2024-05-02 00:25:56 +0530

    ssfs

--- src/components/IndexHome.js
+++ src/components/IndexHome.js
@@ -69,8 +69,64 @@ const IndexHome = () => {
     };
   }, []);
 
+  //Mouse Animation
+
+  useEffect(() => {
+    const cursor = document.querySelector(".cursor");
+    const cursorinner = document.querySelector(".cursor2");
+    const links = document.querySelectorAll("a");
+
+    const onMouseMove = (e) => {
+      const x = e.clientX;
+      const y = e.clientY;
+      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+      cursorinner.style.left = x + "px";
+      cursorinner.style.top = y + "px";
+    };
+
+    const onMouseDown = () => {
+      cursor.classList.add("click");
+      cursorinner.classList.add("cursorinnerhover");
+    };
+
+    const onMouseUp = () => {
+      cursor.classList.remove("click");
+      cursorinner.classList.remove("cursorinnerhover");
+    };
+
+    const onLinkMouseOver = () => {
+      cursor.classList.add("hover");
+    };
+
+    const onLinkMouseLeave = () => {
+      cursor.classList.remove("hover");
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mousedown", onMouseDown);
+    document.addEventListener("mouseup", onMouseUp);
+
+    links.forEach((link) => {
+      link.addEventListener("mouseover", onLinkMouseOver);
+      link.addEventListener("mouseleave", onLinkMouseLeave);
+    });
+
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mousedown", onMouseDown);
+      document.removeEventListener("mouseup", onMouseUp);
+
+      links.forEach((link) => {
+        link.removeEventListener("mouseover", onLinkMouseOver);
+        link.removeEventListener("mouseleave", onLinkMouseLeave);
+      });
+    };
+  }, []);
+
   return (
     <div>
+      <div class="cursor"></div>
+      <div class="cursor2"></div>
       <ThreeDHeader />
       {/* <Header /> */}
 
